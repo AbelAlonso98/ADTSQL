fcfa925702cb7106309c4116f86b196c5951c5be
AbelAlonso98 <abelalonsojimenez@gmail.com>
2023-12-02 15:16:58 +0100

    02/12 EJ2-10 done

--- ADT-SQL/src/main/java/ejercicios2_1/Ejercicio2_10.java
+++ ADT-SQL/src/main/java/ejercicios2_1/Ejercicio2_10.java
@@ -17,6 +17,9 @@ public class Ejercicio2_10 {
 			else {
 				Connection conexion = DriverManager.getConnection(
 						"jdbc:mysql://localhost:3306/ejemplo1? UseSSL=true & serverTimezone =UTC", "root", "4327");
+				
+				// En caso de que emp_no, salario, comision o dept_no no sean un numero salta una
+				// NumberFormatException que es tratada más abajo.
 				int emp_no = Integer.valueOf(args[0]);
 				String apellido = args[1];
 				String oficio = args[2];
@@ -26,7 +29,7 @@ public class Ejercicio2_10 {
 				int dept_no = Integer.valueOf(args[6]);
 
 
-				// Comprobaciones
+				// =========Comprobaciones=========
 				// Salario > 0
 				if (salario < 0) {
 					System.err.println("El salario ha de ser superior a 0€.");
@@ -36,50 +39,50 @@ public class Ejercicio2_10 {
 				Statement sentencia = conexion.createStatement();
 				String sql = String.format("SELECT COUNT(*) FROM DEPARTAMENTOS WHERE DEPT_NO = %s", dept_no);
 				ResultSet rs = sentencia.executeQuery(sql);
-
+				rs.next();
 				if (rs.getInt(0) == 0) {
 					System.err.printf("No existe el departamento: %d%n", dept_no);
 					System.exit(0);
 				}
-
 				// Existe el director
 				sql = String.format("SELECT COUNT(*) FROM EMPLEADOS WHERE DIR = %s", dir);
 				rs = sentencia.executeQuery(sql);
+				rs.next();
 				if (rs.getInt(0) == 0) {
 					System.err.printf("No existe el director: %d%n", dir);
 					System.exit(0);
 				}
-
 				// Apellido != null
 				if (apellido == null || apellido.equals("")) {
 					System.err.println("El apellido no puede ser null.");
 					System.exit(0);
 				}
-
 				// Oficio != null
 				if (oficio == null || oficio.equals("")) {
 					System.err.println("El oficio no puede ser null.");
 					System.exit(0);
 				}
-
 				// Ya existe el empleado
 				sql = String.format("SELECT COUNT(*) FROM EMPLEADOS WHERE EMP_NO = %s", emp_no);
 				rs = sentencia.executeQuery(sql);
+				rs.next();
 				if (rs.getInt(0) > 0) {
 					System.err.printf("Ya existe el empleado: %d%n", emp_no);
 					System.exit(0);
 				}
 
+				// ========= Si todas las comprobaciones son correctas =========
 				sql = String.format("INSERT INTO departamentos VALUES (%d, '%s', '%s', '%s', '%d', '%d', '%d', '%t')",
 						emp_no, apellido, oficio, dir, LocalDate.now(), salario, comision, dept_no);
+				System.out.println("Parametros correctos, ejecutando:");
 				System.out.println(sql);
 
-				int filas = sentencia.executeUpdate(sql);
-				System.out.printf("Filas afectadas: %d%n", filas);
+				sentencia.executeUpdate(sql);
 				sentencia.close();
 				conexion.close();
 			}
 		} catch (SQLException e) {
+			System.err.println("Error de SQL:");
 			e.printStackTrace();
 		} catch (NumberFormatException e) {
 			// Si algun argumento que habia de ser un numero no lo es, se recoge aqui.
